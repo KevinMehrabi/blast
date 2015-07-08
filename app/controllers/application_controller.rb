d46a490ec93a0fc81edfabd8a9b8bd4545b6a801
Kevin Mehrabi <kmehrabi@me.com>
2015-07-07 18:31:14 -0700

    adds before filter and protected method for set_current_ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_current_ip
+
+  protected
+  def set_current_ip
+      User.current_ip = request.env['REMOTE_ADDR']
+  end
+
 end
