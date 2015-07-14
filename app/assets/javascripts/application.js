79d624b1aa1054fcf86b9ec913fdae832ac31d02
Kevin Mehrabi <kmehrabi@me.com>
2015-07-14 10:37:01 -0700

    adds autocomplete feature

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require underscore
-//= require gmaps/google
 //= require_tree .
 
 var x = document.getElementById("demo");
@@ -39,15 +37,3 @@ function setLocation(position){
 
             })
 }
-
-var places, userPlace, $input = $("#place");
-var autocomplete = new google.maps.places.SearchBox($input[0]);
-
-google.maps.event.addListener(autocomplete, 'places_changed', function() {
- var places = autocomplete.getPlaces();
- if (places.length == 0)
-  return;
-
- firstPlace = places[0];
- alert(firstPlace.place_id);
-});
